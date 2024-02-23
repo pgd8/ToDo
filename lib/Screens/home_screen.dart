74e97e7fbb4e010b28ba6a0f8873b643a0931f10
bedoh <abdelrahmanhamdi08@gmail.com>
2024-02-24 01:48:33 +0200

    Bottom navigation is dons and the floating Button

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,11 +1,54 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:todo/Shared_Components/field_label.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName = 'HomeScreen';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('To Do List'),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 10,
+        child: BottomNavigationBar(
+            currentIndex: index,
+            onTap: (value) {
+              index = value;
+              setState(() {});
+            },
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Tasks'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings), label: 'Tasks'),
+            ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+
+          setState(() {});
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 30,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      extendBody: true,
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -3,19 +3,49 @@ import 'package:todo/Screens/home_screen.dart';
 import 'package:todo/Screens/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ToDo',
+      theme: ThemeData(
+          primaryColor: Colors.blue.shade700,
+          scaffoldBackgroundColor: const Color(0xffDFECDB),
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.blue.shade700,
+              centerTitle: true,
+              titleTextStyle: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17.5)),
+          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              showSelectedLabels: true,
+              showUnselectedLabels: false,
+              selectedItemColor: Colors.blue,
+              unselectedItemColor: Colors.grey),
+          bottomAppBarTheme: BottomAppBarTheme(
+            shape: CircularNotchedRectangle(),
+            color: Colors.white,
+            padding: const EdgeInsets.all(0),
+          ),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+              backgroundColor: Colors.blue.shade700,
+              shape: const CircleBorder(
+                  side: BorderSide(color: Colors.white, width: 2))),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue.shade700,
+          )
+        )
+      ),
       home: SplashScreen(),
       initialRoute: SplashScreen.routeName,
       routes: {
-        SplashScreen.routeName: (context)=> SplashScreen(),
-      HomeScreen.routeName: (context) => HomeScreen()
+        SplashScreen.routeName: (context) => SplashScreen(),
+        HomeScreen.routeName: (context) => HomeScreen()
       },
     );
   }
 }
-
